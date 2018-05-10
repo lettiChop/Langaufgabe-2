4dc5f20c1703b255379eca02a7ec4e06459f2d0f
JOy <38735342+joyske@users.noreply.github.com>
2018-05-10 22:48:27 +0200

    Kommentare und Exceptions
    keine Ahnung ob die Exceptions dahin müssen lol

--- Simplex.java
+++ Simplex.java
@@ -6,8 +6,20 @@ public abstract class Simplex
 	public Simplex(int d, Point... points)
 	{
 		dim = d;
+		//Dimenion muss mindestens 0 sein, sonst wird eine Exception geworfen
+		if(dim < 0)
+		{
+			throw new IllegalArgumentException("Die Dimension muss groesser als Null sein!");
+		}
+		//alle Eingabewerte müssen ins Feld passen, daher muss dim mindestens genauso groß sein wie die Anzahl der Eingaben
+		if(dim < p.length)
+		{
+			throw new IllegalArgumentException("Die Dimension ist zu klein fuer die Eingaben!");
+		}
+		//Feld von Objekten der Klasse Point mit der Länge dim+1 wird initialisiert
 		p = new Point[dim+1];
-		for(int i = 0; i < points.length; i++)
+		//Schreiben jeder Eingabe in das zuvor angelegte Feld
+		for(int i = 0; i < dim+1; i++)
 		{
 			p[i] = points[i];
 		}
@@ -15,6 +27,7 @@ public abstract class Simplex
 	
 	public Point get(int i)
 	{
+	        //gibt den Wert der Koordinate an Stelle i des Punktes aus
 		return p[i];
 	}
 	
@@ -23,14 +36,17 @@ public abstract class Simplex
 	public double perimeter()
 	{
 		double leng = 0;
+		//Object der Klasse EuclidDistance zur Berechnung der Seitenlänge wird erstellt
 		EuclidDistance e = new EuclidDistance();
 		for(int i = 0; i < p.length-1; i++)
-		{
+		{ 
+		        //Seitenlänge zwischen zwei jeweils Koordinaten wird berechnet
 			leng += e.distance(p[i], p[i+1]);
 		}
+		//am Ende wird noch die Distanz zwischen der ersten und der letzten Koordinate addiert
 		leng += e.distance(p[0], p[p.length-1]);
 		return leng;
-	}
+}
 	
 	
 	//MAIN METHODE NUR ZUM TESTEN
